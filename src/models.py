9f570e5e9441e5ac880a86738d62c132ee5bcf83
Brian Moya <122998981+brianmoyar89@users.noreply.github.com>
2023-07-22 03:17:26 +0000

    final

--- src/models.py
+++ src/models.py
@@ -7,23 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Users(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    favorites = relationship("Favorites")
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Planets(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planet_name = Column(String(50), nullable=False)
+    galaxy_name = Column(String(50), nullable=False)
+    population = Column(String(50)) 
+
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(50), nullable=False)
+    character_gender = Column(String(50))
+    eyes_color = Column(String(50))
+    hair_color = Column(String(50)) 
+    # character_team = relationship(Person)
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_users = Column(Integer, ForeignKey("users.id"))
+    id_planets = Column(Integer, ForeignKey("planets.id"))
+    id_characters = Column(Integer, ForeignKey("characters.id"))
+    # id_favorites = Column(Integer, ForeignKey('person.id'))
 
     def to_dict(self):
         return {}
